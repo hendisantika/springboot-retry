84625fbeb3f596721bd4a52495f0e4de23fb4412
Hendi Santika <hendisantika@gmail.com>
2020-01-10 06:26:27 +0700

    #5 - Add simpleRetry in BillingService

--- src/main/java/com/hendisantika/springbootretry/BillingService.java
+++ src/main/java/com/hendisantika/springbootretry/BillingService.java
@@ -2,7 +2,11 @@ package com.hendisantika.springbootretry;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
+
+import java.sql.SQLException;
 
 /**
  * Created by IntelliJ IDEA.
@@ -17,4 +21,13 @@ import org.springframework.stereotype.Service;
 public class BillingService {
     private static final Logger LOGGER = LoggerFactory.getLogger(BillingService.class);
     int counter = 0;
+
+    @Retryable(value = {SQLException.class}, maxAttempts = 3, backoff = @Backoff(delay = 5000))
+    public String simpleRetry() throws SQLException {
+        counter++;
+        LOGGER.info("Billing Service Failed " + counter);
+        throw new SQLException();
+
+    }
+
 }
