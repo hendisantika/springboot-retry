87ead81dbda366e1b5b06bc16dad2f5673d2b416
Hendi Santika <hendisantika@gmail.com>
2020-01-10 06:25:31 +0700

    #3 - Add @EnableRetry

--- src/main/java/com/hendisantika/springbootretry/SpringbootRetryApplication.java
+++ src/main/java/com/hendisantika/springbootretry/SpringbootRetryApplication.java
@@ -2,8 +2,10 @@ package com.hendisantika.springbootretry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
+@EnableRetry
 public class SpringbootRetryApplication {
 
     public static void main(String[] args) {
