7172c65c1e77a18659e4e055f5deb48d889a4353
Hendi Santika <hendisantika@gmail.com>
2020-01-10 06:27:17 +0700

    #7 - Add BillingClientService

---
+++ src/main/java/com/hendisantika/springbootretry/BillingClientService.java
@@ -0,0 +1,18 @@
+package com.hendisantika.springbootretry;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-retry
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 10/01/20
+ * Time: 06.26
+ */
+@RestController
+@RequestMapping(value = "/billing")
+public class BillingClientService {
+}
